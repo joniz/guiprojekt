55e1a538883aaac3fedbc4c3c0d5d4619fe9ee5e
hugoqqqq <oscar.mlinaric@hotmail.com>
2017-01-17 16:48:27 +0100

    fixat systemtray lite halvt, systemtray aktiveras när fönstret inte syns

--- guiprojekt/guiprojekt/MainWindow.xaml.cs
+++ guiprojekt/guiprojekt/MainWindow.xaml.cs
@@ -12,18 +12,38 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
 
 namespace guiprojekt
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+        private System.Windows.Forms.NotifyIcon MyNotifyIcon;
+
         public MainWindow()
+                {
+                    InitializeComponent();
+                    MyNotifyIcon = new System.Windows.Forms.NotifyIcon();
+                    MyNotifyIcon.Icon = new System.Drawing.Icon(@"C:\labbar\GUIProjekt\guiprojekt\ReminderIcon.ico",16,16);
+                    MyNotifyIcon.MouseDoubleClick +=
+                        new System.Windows.Forms.MouseEventHandler
+                            (MyNotifyIcon_MouseDoubleClick);
+                }
+
+
+
+        void MyNotifyIcon_MouseDoubleClick(object sender,System.Windows.Forms.MouseEventArgs e)
         {
-            InitializeComponent();
+            this.WindowState = WindowState.Normal;
+            MyNotifyIcon.Visible = false;
+            this.ShowInTaskbar = true;
         }
+
+        
+
+
+
 
         private void monthPicker_Loaded(object sender, RoutedEventArgs e)
         {
@@ -34,5 +54,25 @@ namespace guiprojekt
         {
 
         }
+
+
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            
+        }
+
+      
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            this.ShowInTaskbar = false;
+            MyNotifyIcon.BalloonTipTitle = "Minimize Sucessful";
+            MyNotifyIcon.BalloonTipText = "Minimized the app ";
+            MyNotifyIcon.ShowBalloonTip(400);
+            MyNotifyIcon.Visible = true;
+        }
+
+       
+
     }
 }
