171449ebeec3967ec58eff7a68456a943e0e6eb8
joniz <jaej96@gmail.com>
2017-01-22 15:59:56 +0100

    awdawd

--- guiprojekt/guiprojekt/reminder.cs
+++ guiprojekt/guiprojekt/reminder.cs
@@ -15,36 +15,22 @@ namespace guiprojekt
         private string _title;
         private DateTime _startTime;
         private DateTime _alarmTime;
-<<<<<<< HEAD
-        private WeekDays _value;
-        public enum WeekDays
-        {
-            Sunday,
-            Monday,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday
-
-
-        }        
-        public reminder(string title, DateTime startTime, DateTime alarmTime, WeekDays day)
-=======
         private int _valueOfWeekDay;
+        
+       
+
 
                 
         public reminder(string title, DateTime startTime, DateTime alarmTime, DayOfWeek days)
->>>>>>> c66dc99b726efa331c82c94810b4773bd6b0349d
+
         {
             _title = title;
             _startTime = startTime;
             _alarmTime = alarmTime;
-<<<<<<< HEAD
-            _value = day;
-=======
+
+            
             _valueOfWeekDay = (int)days;
->>>>>>> c66dc99b726efa331c82c94810b4773bd6b0349d
+
             
             
 
