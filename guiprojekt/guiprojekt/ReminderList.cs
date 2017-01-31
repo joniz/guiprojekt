e775941e3ab7eac69d674969e1b6a471d566726a
JokaKlintan <johannes.klint@hotmail.com>
2017-01-31 13:52:09 +0100

    haha

---
+++ guiprojekt/guiprojekt/ReminderList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace guiprojekt
+{
+    class ReminderList
+    {
+        List<reminder> _reminderList = new List<reminder>();
+
+        public void addReminder()
+        {
+
+        }
+
+
+        public void writeToFile(reminder remObj)
+        {
+            using (Stream stream = File.Open("remindersBin.bin", FileMode.Create))
+            {
+
+                BinaryFormatter bin = new BinaryFormatter();
+                bin.Serialize(stream, remObj);
+
+
+                /* outputFile.WriteLine("");
+                 outputFile.Write(remObj._title);
+                 outputFile.Write("|");
+                 outputFile.Write(remObj._startTime.Hour.ToString());
+                 outputFile.Write(":");
+                 outputFile.Write(remObj._startTime.Minute.ToString());
+                 outputFile.Write("|");
+                 outputFile.Write(remObj._alarmTime.Hour.ToString());
+                 outputFile.Write(":");
+                 outputFile.Write(remObj._alarmTime.Minute.ToString());
+                 outputFile.Write("|");
+                 for (int i = 0; remObj._weekDays.Count > i; i++)
+                 {
+                     outputFile.Write(remObj._weekDays[i]);
+                     outputFile.Write("|");
+                 }
+
+             }
+             */
+
+
+
+
+
+          
+            }
+        }
+    }
+}
--- guiprojekt/guiprojekt/reminder.cs
+++ guiprojekt/guiprojekt/reminder.cs
@@ -10,12 +10,12 @@ using System.Runtime.Serialization.Formatters.Binary;
 namespace guiprojekt
 {
     [Serializable]
-    class reminder
+    public class reminder
     {
         public string _title { get; set; }
         public DateTime _startTime { get; set; }
         public DateTime _alarmTime { get; set; }
-
+        public int _idNum;
         public List<DayOfWeek> _weekDays { get; set; }
        
 
@@ -29,32 +29,70 @@ namespace guiprojekt
             _alarmTime = Convert.ToDateTime(alarmTime);
             _weekDays = days;
 
-            
-                 
         
         }
-
-
-
-    public void saveToFile()
+        public static void writeToFile(List<reminder> reminderList)
         {
 
+            using (Stream stream = File.Open("remindersBin.bin", FileMode.Create))
+            {
 
+                BinaryFormatter bin = new BinaryFormatter();
+                bin.Serialize(stream, reminderList);
+            }
+
+            /*  outputFile.WriteLine("");
+               outputFile.Write(remObj._title);
+               outputFile.Write("|");
+               outputFile.Write(remObj._startTime.Hour.ToString());
+               outputFile.Write(":");
+               outputFile.Write(remObj._startTime.Minute.ToString());
+               outputFile.Write("|");
+               outputFile.Write(remObj._alarmTime.Hour.ToString());
+               outputFile.Write(":");
+               outputFile.Write(remObj._alarmTime.Minute.ToString());
+               outputFile.Write("|");
+               for (int i = 0; remObj._weekDays.Count > i; i++)
+               {
+                   outputFile.Write(remObj._weekDays[i]);
+                   outputFile.Write("|");
+               }
+               */
+        }
+        public List<reminder> readFromFile()
+        {
+            if (File.Exists(@"remindersBin.bin"))
+            {
+                using (Stream stream = File.Open(@"remindersBin.bin", FileMode.Open))
+                {
+                    BinaryFormatter bin = new BinaryFormatter();
+                    List<reminder> _listWithAllReminders = (List<reminder>)bin.Deserialize(stream);
+                    return _listWithAllReminders;
+                    
+                }
+               
+
+
+
+            }
+            return null;
         }
 
 
-        
-       
 
 
 
-     
 
 
 
-    
 
-    
+
+
+
+
+
+
+
+        }
     }
-}
  
